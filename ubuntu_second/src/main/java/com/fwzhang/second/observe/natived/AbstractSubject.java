31fbd9b2021192f1098a66fbdd13e4c6256bea8e
fwzhang <2372590227@qq.com>
2021-04-06 09:53:25 +0800

    feat: 观察者模式

---
+++ ubuntu_second/src/main/java/com/fwzhang/second/observe/natived/AbstractSubject.java
@@ -0,0 +1,25 @@
+package com.fwzhang.second.observe.natived;
+
+/**
+ * 被观察者
+ */
+public interface AbstractSubject<T> {
+
+    /**
+     * 订阅观察者
+     * @param observer
+     */
+    void addObserver(T observer);
+
+    /**
+     * 观察者取消订阅
+     * @param observer
+     */
+    void removeObserver(T observer);
+
+    /**
+     * 通知观察者
+     */
+    void notifyObserver();
+
+}
---
+++ ubuntu_second/src/main/java/com/fwzhang/second/observe/natived/Observer.java
@@ -0,0 +1,7 @@
+package com.fwzhang.second.observe.natived;
+
+/**
+ * 抽象观察者
+ */
+public interface Observer {
+}
