69ae2ccf41e5f3651bbbd2bfc72f134d457a9658
fwzhang <2372590227@qq.com>
2021-06-08 22:01:10 +0800

    feat: AbstractCollection class code

--- ubuntu_third/src/main/java/com/fwzhang/third/collectionframework/collection/AbstractCollectionClass.java
+++ ubuntu_third/src/main/java/com/fwzhang/third/collectionframework/collection/AbstractCollectionClass.java
@@ -1,6 +1,8 @@
 package com.fwzhang.third.collectionframework.collection;
 
+import java.util.Collection;
 import java.util.Iterator;
+import java.util.Objects;
 
 /**
  * @ClassName AbstractCollectionClass
@@ -20,10 +22,167 @@ public abstract class AbstractCollectionClass {
 
     /**
      * 定义的抽象方法,要求子类需要实现这两个方法
+     * AbstractCollection类中很多方法都会在方法内部调用这两个方法
+     * 这就要求子类需要实现这两个抽象方法,从而实现对应的功能
      * @return
      */
     public abstract Iterator iterator();
 
     public abstract int size();
 
+    /**
+     * 判断集合是否为空
+     * 使用到了size()
+     * @return
+     */
+    public boolean isEmpty() {
+        return size() == 0;
+    }
+
+    /**
+     * 添加元素,不支持该操作
+     * @param o
+     * @return
+     */
+    public boolean add(Object o) {
+        throw new UnsupportedOperationException("不支持该操作");
+    }
+
+    public <E> boolean addAll(Collection<? extends E> c) {
+        boolean modified = false;
+        for (E e : c)
+            if (add(e))
+                modified = true;
+        return modified;
+    }
+
+    /**
+     * 判断集合中是否包含某个对象
+     * 使用了iterator()方法
+     * @param o
+     * @return
+     */
+    public boolean contains(Object o) {
+        Iterator it = iterator();
+        if (o==null) {
+            while (it.hasNext())
+                if (it.next()==null)
+                    return true;
+        } else {
+            while (it.hasNext())
+                // 自定义对象
+                // 如果需要特殊的判断逻辑,需要重写equals和hashcode方法
+                if (o.equals(it.next()))
+                    return true;
+        }
+        return false;
+    }
+
+    /**
+     * 循环调用contains方法
+     * @param c
+     * @return
+     */
+    public boolean containsAll(Collection<?> c) {
+        for (Object e : c)
+            if (!contains(e))
+                return false;
+        return true;
+    }
+
+    /**
+     * 移除集合内某个元素
+     * 使用了iterator方法
+     * @param o
+     * @return
+     */
+    public boolean remove(Object o) {
+        Iterator it = iterator();
+        if (o==null) {
+            while (it.hasNext()) {
+                if (it.next()==null) {
+                    it.remove();
+                    return true;
+                }
+            }
+        } else {
+            while (it.hasNext()) {
+                if (o.equals(it.next())) {
+                    it.remove();
+                    return true;
+                }
+            }
+        }
+        return false;
+    }
+
+    /**
+     * 使用iterator的remove方法
+     * 删除指定集合中包含在本集合中的元素
+     * @param c
+     * @return
+     */
+    public boolean removeAll(Collection<?> c) {
+        Objects.requireNonNull(c);
+        boolean modified = false;
+        Iterator<?> it = iterator();
+        while (it.hasNext()) {
+            if (c.contains(it.next())) {
+                it.remove();
+                modified = true;
+            }
+        }
+        return modified;
+    }
+
+    /**
+     * 删除指定集合中不在本集合中的元素
+     * 即保留共有的元素
+     * 与removeAll方法相反
+     * @param c
+     * @return
+     */
+    public boolean retainAll(Collection<?> c) {
+        Objects.requireNonNull(c);
+        boolean modified = false;
+        Iterator it = iterator();
+        while (it.hasNext()) {
+            if (!c.contains(it.next())) {
+                it.remove();
+                modified = true;
+            }
+        }
+        return modified;
+    }
+
+    /**
+     * 清空集合
+     */
+    public void clear() {
+        Iterator it = iterator();
+        while (it.hasNext()) {
+            it.next();
+            it.remove();
+        }
+    }
+
+    /**
+     * 集合String输出打印
+     * @return
+     */
+    public String toString() {
+        Iterator it = iterator();
+        if (! it.hasNext())
+            return "[]";
+
+        StringBuilder sb = new StringBuilder();
+        sb.append('[');
+        for (;;) {
+            Object e = it.next();
+            sb.append(e == this ? "(this Collection)" : e);
+            if (! it.hasNext())
+                return sb.append(']').toString();
+            sb.append(',').append(' ');
+        }
+    }
 }
