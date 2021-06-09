feaec83da6f5f28e16bb7b2a21c50eadd7eef452
zhangfengwei <zhangfengwei@magfin.cn>
2021-06-09 18:00:22 +0800

    feat: AbstractCollection class code

--- ubuntu_third/src/main/java/com/fwzhang/third/collectionframework/collection/AbstractCollectionClass.java
+++ ubuntu_third/src/main/java/com/fwzhang/third/collectionframework/collection/AbstractCollectionClass.java
@@ -1,5 +1,6 @@
 package com.fwzhang.third.collectionframework.collection;
 
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.Iterator;
 import java.util.Objects;
@@ -184,5 +185,94 @@ public abstract class AbstractCollectionClass {
                 return sb.append(']').toString();
             sb.append(',').append(' ');
         }
+    }
+
+    /**
+     * 将集合转换为数组
+     * @return
+     */
+    public Object[] toArray() {
+        // Estimate size of array; be prepared to see more or fewer elements
+        // 首先以现有的size()创建数组
+        Object[] r = new Object[size()];
+        Iterator it = iterator();
+        // 遍历数组，通过iterator的next遍历集合
+        // 使用数据遍历，是为了防止在toArray过程中，集合元素增加，从而导致索引溢出
+        for (int i = 0; i < r.length; i++) {
+            if (! it.hasNext()) // fewer elements than expected
+                // iterator与size()长度不一致，iterator长度较小时
+                // 拷贝当前的数组返回
+                return Arrays.copyOf(r, i);
+            r[i] = it.next();
+        }
+        // 判断iterator的长度是否大于size，不大于返回数组r,大于就调用finnishToArray方法将剩余的元素收集进数组中
+        return it.hasNext() ? finishToArray(r, it) : r;
+    }
+
+    public <T> T[] toArray(T[] a) {
+        // Estimate size of array; be prepared to see more or fewer elements
+        int size = size();
+        T[] r = a.length >= size ? a :
+                (T[])java.lang.reflect.Array
+                        .newInstance(a.getClass().getComponentType(), size);
+        Iterator it = iterator();
+
+        for (int i = 0; i < r.length; i++) {
+            if (! it.hasNext()) { // fewer elements than expected
+                if (a == r) {
+                    r[i] = null; // null-terminate
+                } else if (a.length < i) {
+                    return Arrays.copyOf(r, i);
+                } else {
+                    System.arraycopy(r, 0, a, 0, i);
+                    if (a.length > i) {
+                        a[i] = null;
+                    }
+                }
+                return a;
+            }
+            r[i] = (T)it.next();
+        }
+        // more elements than expected
+        return it.hasNext() ? finishToArray(r, it) : r;
+    }
+
+    // 最大数据长度
+    // 数组作为一个对象，存在一个对象头信息，最大不超过8个字节
+    private static final int MAX_ARRAY_SIZE = Integer.MAX_VALUE - 8;
+
+    /**
+     * 收集多余元素到数组中
+     * @param r
+     * @param it
+     * @param <T>
+     * @return
+     */
+    private static <T> T[] finishToArray(T[] r, Iterator<?> it) {
+        int i = r.length;
+        while (it.hasNext()) {
+            int cap = r.length;
+            if (i == cap) {
+                // 数组长度扩展
+                // newLength = old + old/2 + 1
+                int newCap = cap + (cap >> 1) + 1;
+                // overflow-conscious code
+                if (newCap - MAX_ARRAY_SIZE > 0)
+                    newCap = hugeCapacity(cap + 1);
+                r = Arrays.copyOf(r, newCap);
+            }
+            r[i++] = (T)it.next();
+        }
+        // trim if overallocated
+        return (i == r.length) ? r : Arrays.copyOf(r, i);
+    }
+
+    private static int hugeCapacity(int minCapacity) {
+        if (minCapacity < 0) // overflow
+            throw new OutOfMemoryError
+                    ("Required array size too large");
+        return (minCapacity > MAX_ARRAY_SIZE) ?
+                Integer.MAX_VALUE :
+                MAX_ARRAY_SIZE;
     }
 }
