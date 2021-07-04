3aa4d51650db635b478ebebb6c4948498a76fd09
fwzhang <2372590227@qq.com>
2021-07-04 17:51:22 +0800

    feat: LinkedList class code

--- ubuntu_third/src/main/java/com/fwzhang/third/collectionframework/collection/LinkedListClass.java
+++ ubuntu_third/src/main/java/com/fwzhang/third/collectionframework/collection/LinkedListClass.java
@@ -13,6 +13,49 @@ import java.util.*;
 
 public class LinkedListClass<E> extends AbstractSequentialList<E>
         implements List<E>, Deque<E>, Cloneable, Serializable {
+
+    /**
+     * LinkedList实质为一个链表
+     * 双向链表,没有循环,即最后一个元素的next没有指向第一个元素
+     * 每一个节点都拥有一个指向前一个和后一个元素的指针
+     */
+
+    /**
+     * LinkedList类用于存储数据的节点类
+     *
+     * @param <E>
+     */
+    private static class Node<E> {
+        E item;
+        // 指向前一个和后一个的指针
+        Node<E> next;
+        Node<E> prev;
+
+        public Node(Node<E> prev, E item, Node<E> next) {
+            this.item = item;
+            this.next = next;
+            this.prev = prev;
+        }
+    }
+
+    // LinkedList的属性
+    transient int size = 0; // 容量
+    transient Node<E> first; // 链表第一个元素
+    transient Node<E> last; // 链表最后一个元素
+
+    // 构造函数
+
+    /**
+     * LinkedList仅支持两种构造方式
+     * 其中无参构造函数中没有任何操作,仅建立LinkedList对象,当前链表中无数据,size=0,first和last均为null
+     * 含参构造函数,将Collection类型的数据转换为LinkedList类型
+     */
+    public LinkedListClass() { }
+    public LinkedListClass(Collection<? extends E> c) {
+        this();
+        addAll(c);
+    }
+
     @Override
     public ListIterator<E> listIterator(int index) {
         return null;
